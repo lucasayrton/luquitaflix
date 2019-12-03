e49a4522acd5e97995151827832d14f7d3001541
Lucas Ayrton <lucasayrton66@gmail.com>
2019-12-03 08:55:45 -0300

    Pequenos ajustes

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 setTimeout(function () {
-  var video = document.getElementById("video-background");
-  video.classList.add("video-fadeIn");
+  var video = document.getElementById('videoBackground');
+  video.classList.add('video-fadeIn');
   video.play();
 }, 5000);
 
 function slideLeft() {
-  var elmnt = document.getElementById("slide");
+  var elmnt = document.getElementById('slide');
   elmnt.scrollBy({ top: 0, left: -250, behavior: 'smooth' });
 }
 
 function slideRight() {
-  var elmnt = document.getElementById("slide");
+  var elmnt = document.getElementById('slide');
   elmnt.scrollBy({ top: 0, left: 250, behavior: 'smooth' });
 }
